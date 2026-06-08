8e96151704319afbe0b8400940a8d2c40a740c9b
h2st0ry <yeonhee040512@gmail.com>
2026-06-08 19:07:16 +0900

    Revert "Feat: kakao map API를 받아오기 위한 로직 생성 및 Security 추가"
    This reverts commit b425baa1191f18b91b9e1f120a525040b8cfbf44.

--- src/main/java/com/carrot/munaro/security/SecurityConfig.java
+++ src/main/java/com/carrot/munaro/security/SecurityConfig.java
@@ -46,11 +46,6 @@ public class SecurityConfig {
                                 "/",
                                 "/auth/**"
                         ).permitAll()
-
-                        .requestMatchers(
-                                "/api/tourist-spots/**"
-                        ).permitAll()
-
                         .anyRequest().authenticated()
                 )
 
--- src/main/java/com/carrot/munaro/tourist_spot/controller/TouristSpotController.java
+++ src/main/java/com/carrot/munaro/tourist_spot/controller/TouristSpotController.java
@@ -1,23 +1,11 @@
 package com.carrot.munaro.tourist_spot.controller;
 
-import com.carrot.munaro.tourist_spot.service.KakaoLocalClient;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/tourist-spots")
 @RequiredArgsConstructor
 public class TouristSpotController {
-
-    private final KakaoLocalClient kakaoLocalClient;
-
-    @GetMapping("/search")
-    public String search(
-            @RequestParam String keyword
-    ) {
-        return kakaoLocalClient.searchPlace(keyword);
-    }
 }
